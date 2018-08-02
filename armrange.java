33215ded2589b0b24413208ccd0f311aa4f27f71
AnaghaNarla <41769687+AnaghaNarla@users.noreply.github.com>
2018-08-02 14:52:05 +0530

    Create armrange.java

---
+++ armrange.java
@@ -0,0 +1,35 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int low=150,high=160;
+		for(int number=low+1;number<high;number++)
+		{
+			int digits=0;
+			int result=0;
+			int originalnumber=number;
+			while(originalnumber!=0)
+			{
+				originalnumber/=10;
+				digits++;
+			}
+			originalnumber=number;
+			while(originalnumber!=0)
+			{
+				int remainder=originalnumber%10;
+				result+=Math.pow(remainder,digits);
+				originalnumber/=10;
+			}
+			if(result==number)
+			System.out.println(number + " ");
+		}
+	}
+}
