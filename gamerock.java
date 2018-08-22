70e8bce44bb44b66cde57fb2e470ac057e63e795
AnaghaNarla <41769687+AnaghaNarla@users.noreply.github.com>
2018-08-22 14:50:24 +0530

    Create gamerock.java

---
+++ gamerock.java
@@ -0,0 +1,47 @@
+ copy
+/* package whatever; // don't place package name! */
+ 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	final static int ROCK = 1, SCISSOR = 2, PAPER = 3;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scan = new Scanner(System.in);
+        System.out.println("Player 1: Choose (1) - Rock, (2) - Scissors, or (3) - Paper: ");
+        int player1 = scan.nextInt();
+        System.out.println("Player 2: Choose (1) - Rock, (2) - Scissors, or (3) - Paper: ");
+        int player2 = scan.nextInt();
+ 
+        if (player1 == player2)
+        {
+            System.out.print("It is a tie");
+        } else {
+            switch (player1){
+            case ROCK:
+                if (player2 == SCISSOR)
+                    System.out.print("Player 1 wins!");
+                else
+                    System.out.print("Player 2 wins!");
+                break;
+            case SCISSOR:
+                if (player2 == PAPER)
+                    System.out.print("Player 1 wins!");
+                else
+                    System.out.print("Player 2 wins!");
+                break;
+            case PAPER:
+                if (player2 == ROCK)
+                    System.out.print("Player 1 wins!");
+                else
+                    System.out.print("Player 2 wins!");
+                break;
+            }
+	}
+	}
+}
