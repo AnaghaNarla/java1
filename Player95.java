36a387d0f1f2e02ec2c952b75bab95b2954029f4
AnaghaNarla <41769687+AnaghaNarla@users.noreply.github.com>
2018-09-07 14:50:08 +0530

    Create Player95.java

---
+++ Player95.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int i;
+		for(i=0;i<s.length();i++)
+		{
+			if(i==n)
+		System.out.print(s.charAt((i-1)+k));
+		}
+	}
+}
